3a94f70fcd2cdbb7bc7b4059972402f44ffd9fad
Isai Tobar <isai.tobar@hotmail.com>
2026-04-02 01:40:57 -0500

    feat: add S3_WEB_UPSTREAM variable to Nginx entrypoint configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -eu
 
 # CDN Nginx — Entrypoint
 # envsubst ONLY listed vars; Nginx runtime vars ($uri etc.) stay intact.
-VARS='$IMAGOR_UPSTREAM $S3_UPSTREAM $S3_BUCKET $ALLOWED_ORIGINS_REGEX $CACHE_MAX_SIZE $CDN_DOMAIN $MAX_VIDEO_SIZE $MAX_IMG_SIZE'
+VARS='$IMAGOR_UPSTREAM $S3_UPSTREAM $S3_WEB_UPSTREAM $S3_BUCKET $ALLOWED_ORIGINS_REGEX $CACHE_MAX_SIZE $CDN_DOMAIN $MAX_VIDEO_SIZE $MAX_IMG_SIZE'
 
 envsubst "$VARS" < /etc/nginx/nginx.conf.template        > /etc/nginx/nginx.conf
 envsubst "$VARS" < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
